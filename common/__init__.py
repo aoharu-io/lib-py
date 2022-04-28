27154a6a5e93bbd690b77bf3e6466d556cc0f126
tasuren <tasuren_0618@yahoo.co.jp>
2022-04-29 08:52:58 +0900

    update: set_handlerを作った。

--- common/__init__.py
+++ common/__init__.py
@@ -1,5 +1,16 @@
 # rtlib - Main
 
+import logging
+
 from . import utils
 
-__all__ = ("utils",)
+
+__all__ = ("utils", "set_handler")
+
+
+def set_handler(logger: logging.Logger) -> None:
+    "渡された`Logger`でログを標準出力に出力するようにします。"
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("[%(name)s] [%(levelname)s] %(message)s"))
+    logger.addHandler(handler)
