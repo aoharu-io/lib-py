b707ef05dc53fdd771e44729194960326f9f6f1e
tasuren <tasuren_0618@yahoo.co.jp>
2022-05-01 09:13:51 +0900

    update: code_blockを実装した。

--- common/utils.py
+++ common/utils.py
@@ -3,9 +3,14 @@
 from traceback import TracebackException
 
 
-__all__ = ("make_error_message",)
+__all__ = ("make_error_message", "code_block")
 
 
 def make_error_message(error: Exception) -> str:
     "渡されたエラーから全文を作ります。"
     return "".join(TracebackException.from_exception(error).format())
+
+
+def code_block(code: str, type_: str = "") -> str:
+    "渡された文字列をコードブロックで囲みます。"
+    return f"```{type_}\n{code}\n```"
