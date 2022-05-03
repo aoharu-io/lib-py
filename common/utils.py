24e9f01ac054e0b0d9e0ba99263cfe32cdcb3c61
tasuren <tasuren_0618@yahoo.co.jp>
2022-05-03 12:29:32 +0900

    update: データクラス用のto_dictを実装した。

--- common/utils.py
+++ common/utils.py
@@ -1,10 +1,9 @@
 # rtlib - Utils
 
-from collections.abc import AsyncIterator
+from typing import Any
+from collections.abc import Callable
 
 from traceback import TracebackException
-
-from aiomysql import Cursor
 
 
 __all__ = ("make_error_message", "code_block")
@@ -18,3 +17,9 @@ def make_error_message(error: Exception) -> str:
 def code_block(code: str, type_: str = "") -> str:
     "渡された文字列をコードブロックで囲みます。"
     return f"```{type_}\n{code}\n```"
+
+
+to_dict_for_dataclass: Callable[..., dict[str, Any]] = lambda self: {
+    key: getattr(self, key) for key in self.__class__.__annotations__.keys()
+}
+"データクラスのデータを辞書として出力する`to_dict`を作成します。"
