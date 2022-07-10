3d7053acbc70adac38b709108e0e5e8515a9a282
tasuren <tasuren_0618@yahoo.co.jp>
2022-07-10 19:52:42 +0900

    update: エラー発生で返信をするためのエラーを作った。

---
+++ common/reply_error.py
@@ -0,0 +1,27 @@
+# rtlib - Reply Error
+
+from typing import Any
+
+
+__all__ = ("ReplyError", "BadRequest", "NotFound")
+
+
+class ReplyError(Exception):
+    "エラーを発生させて返信をするというのに使うためのエラーです。"
+
+    status = 400
+
+    def __init__(
+        self, text: str | dict[str, str] | Any,
+        status: int | None = None, *args, **kwargs
+    ):
+        self.text = text
+        if status is not None:
+            self.status = status
+        super().__init__(self.text, *args, **kwargs)
+
+
+class BadRequest(ReplyError):
+    ...
+class NotFound(ReplyError):
+    status = 404
