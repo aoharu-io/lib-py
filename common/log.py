f9600ad44892e38a0102c04ff4b742dc7c46268f
tasuren <tasuren_0618@yahoo.co.jp>
2022-08-06 07:53:51 +0900

    update: ファイル出力をset_handlerから分離した。

--- common/log.py
+++ common/log.py
@@ -12,6 +12,24 @@ NORMAL_FORMATTER = logging.Formatter(BASE_FORMAT)
 EXTENDED_FORMATTER = logging.Formatter(f"[%(asctime)s] {BASE_FORMAT}", "%Y-%m-%d %H:%M:%S")
 
 
+def set_output_handler(logger: logging.Logger) -> None:
+    "ファイル出力をロガーに設定します。"
+    if not exists("data/logs"):
+        mkdir("data/logs")
+    more = ""
+    if exists("core/bot.py"):
+        # もしBotによるログ出力ならシャードIDを出力先ファイルの名前に入れる。
+        from core.tdpocket import bot # type: ignore
+        assert bot is not None
+        more = bot.rtws.id_
+    handler = RotatingFileHandler(
+        filename="data/logs/rt%s.log" % more, encoding='utf-8', mode='w',
+        maxBytes=32 * 1024 * 1024, backupCount=10
+    )
+    handler.setFormatter(EXTENDED_FORMATTER)
+    logger.addHandler(handler)
+
+
 def set_handler(logger: logging.Logger, output_file: bool = True) -> None:
     "渡された`Logger`でログを標準出力に出力するようにします。オプションでファイル出力します。"
     logger.setLevel(logging.INFO)
@@ -19,17 +37,4 @@ def set_handler(logger: logging.Logger, output_file: bool = True) -> None:
     handler.setFormatter(NORMAL_FORMATTER)
     logger.addHandler(handler)
     if output_file:
-        if not exists("data/logs"):
-            mkdir("data/logs")
-        more = ""
-        if exists("core/bot.py"):
-            # もしBotによるログ出力ならシャードIDを出力先ファイルの名前に入れる。
-            from core.tdpocket import bot
-            assert bot is not None
-            more = bot.rtws.id_
-        handler = RotatingFileHandler(
-            filename="data/logs/rt%s.log" % more, encoding='utf-8', mode='w',
-            maxBytes=32 * 1024 * 1024, backupCount=10
-        )
-        handler.setFormatter(EXTENDED_FORMATTER)
-        logger.addHandler(handler)
+        set_output_handler(logger)
