a625e5144e35558df6ca9e5a62aadca4b6e98d0c
Takagi Tasuku <tasuren@outlook.jp>
2023-01-14 13:45:15 +0900

    fix: デコレータを使うと動かなくなるのとが動かないのを修正。

--- common/database.py
+++ common/database.py
@@ -1,7 +1,7 @@
 # rextlib - Data Manager
 
-from typing import TypeVar, Self
-from collections.abc import AsyncIterator
+from typing import TypeVar, Self, Any
+from collections.abc import AsyncIterator, Callable
 
 from inspect import iscoroutinefunction, isasyncgenfunction, getsource, getfile
 
@@ -24,7 +24,7 @@ async def fetchstep(
     cursor: Cursor, sql: str,
     args: tuple | None = None,
     cycle: int = 50
-) -> AsyncIterator[tuple]:
+) -> AsyncIterator[Any]:
     "少しずつデータベースからデータを読み込みます。(`LIMIT`が使われます。)"
     now, rows = 0, (1,)
     while rows:
@@ -49,43 +49,73 @@ class DatabaseManager:
                 # `cursor`の引数を増設する。
                 l = {}
                 source = getsource(value).replace("self",  "self, cursor", 1)
-                if_count = int(len(source[:source.find("d")]) / 4) - 1
+                index = source.find("@")
+                if index == -1:
+                    index = source.find("async def ")
+
+                if_count = int(index / 4)
                 source = "{}{}".format(
                     "\n" * (value.__code__.co_firstlineno - if_count - 1), "\n".join((
                     "\n".join(f"{'    '*i}if True:" for i in range(if_count)), source
                 )))
                 exec(compile(source, getfile(value), "exec"), value.__globals__, l)
+
                 # 新しい関数を作る。
+                cursors = getattr(value, "__dm_cursor_classes__") if \
+                    hasattr(value, "__dm_cursor_classes__") else ()
                 if gen:
                     @wraps(l[key])
                     async def _new( # type: ignore
-                        self: DatabaseManager, *args, __dm_func__=l[key], **kwargs
+                        self: DatabaseManager, *args,
+                        __dm_func__=l[key],
+                        __dm_cursors__=cursors,
+                        **kwargs
                     ):
                         if "cursor" in kwargs:
-                            async for data in __dm_func__(self, kwargs.pop("cursor"), *args, **kwargs):
+                            async for data in __dm_func__(
+                                self, kwargs.pop("cursor"),
+                                *args, **kwargs
+                            ):
                                 yield data
                         else:
                             async with self.db.acquire() as conn:
-                                async with conn.cursor() as cursor:
-                                    async for data in __dm_func__(self, cursor, *args, **kwargs):
+                                async with conn.cursor(*__dm_cursors__) as cursor:
+                                    async for data in __dm_func__(
+                                        self, cursor, *args, **kwargs
+                                    ):
                                         yield data
                 else:
                     @wraps(l[key])
                     async def _new(
-                        self: DatabaseManager, *args, __dm_func__=l[key], **kwargs
+                        self: DatabaseManager, *args,
+                        __dm_func__=l[key],
+                        __dm_cursors__=cursors,
+                        **kwargs
                     ):
                         if "cursor" in kwargs:
-                            return await __dm_func__(self, kwargs.pop("cursor"), *args, **kwargs)
+                            return await __dm_func__(
+                                self, kwargs.pop("cursor"),
+                                *args, **kwargs
+                            )
                         else:
                             async with self.db.acquire() as conn:
-                                async with conn.cursor() as cursor:
+                                async with conn.cursor(*__dm_cursors__) as cursor:
                                     return await __dm_func__(self, cursor, *args, **kwargs)
                 setattr(cls, key, _new)
 
     @staticmethod
-    def ignore(func: CaT) -> CaT:
+    def set_to_ignore(func: CaT) -> CaT:
+        "メソッドをデータベース使わない関数とするデコレータです。"
         setattr(func, "__dm_ignore__", True)
         return func
+
+    @staticmethod
+    def set_cursor_cls(*cursor_clses: type[Cursor]) -> Callable[[CaT], CaT]:
+        "カーソルのクラスを変更するためのデコレータです。"
+        def decorator(func: CaT) -> CaT:
+            setattr(func, "__dm_cursor_classes__", cursor_clses)
+            return func
+        return decorator
 
 
 @dataclass
