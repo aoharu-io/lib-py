3149470b2b6ec2159f5838c9c5308abbb66f75ea
Takagi Tasuku <tasuren@outlook.jp>
2023-03-01 16:46:59 +0900

    new: ハッシュを作成するためのプログラムを作成。

---
+++ common/hash.py
@@ -0,0 +1,16 @@
+"Rext Lib - File Hash"
+
+__all__ = ("get_hash", "get_file_hash")
+
+from hashlib import sha256
+
+
+def get_hash(data: bytes) -> str:
+    "渡されたデータのハッシュを取得します。"
+    return sha256(data).hexdigest()
+
+
+def get_file_hash(file_path: str) -> str:
+    "ファイルのハッシュを取得します。"
+    with open(file_path, "rb") as f:
+        return get_hash(f.read())
